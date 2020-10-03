81c92d6fd9612b1964628d8a59f495ad0ed9180d
Max <maxdillongendev@gmail.com>
2020-10-03 20:32:54 +0330

    django

--- web_01/settings.py
+++ web_01/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-BASE_DIR=Path(__file__).resolve().parent.parent
+BASE_DIR=os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY='v+9%l@@6upn5j(a#w^sp+$y4-am252o6_!d(8ab4l_!fn!)ah&'
 DEBUG=True
 ALLOWED_HOSTS=['127.0.0.1','ircement.herokuapp.com']
@@ -75,5 +75,5 @@ USE_TZ=True
 STATIC_URL='/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-# STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
